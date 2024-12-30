14bdaafa049a556e5c3881e5c417515534ddb62f
goh jing yang <gohyang@graduate.utm.my>
2024-12-30 15:09:23 +0000

    JY

---
+++ warehouse/webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("warehouse.controller.View1", {
+        onInit() {
+        }
+    });
+});
---
+++ warehouse/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"warehouse/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
